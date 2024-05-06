d176bdad86d70915b574677fd50e3dddd57275a1
HOANG Trung Thong <thoang5@mantu.com>
2024-05-06 18:12:51 +0700

    connect with database

---
+++ Mps.Domain/Entities/MpsDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Mps.Domain.Entities
+{
+    public class MpsDbContext(DbContextOptions<MpsDbContext> options) : DbContext(options)
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasKey(u => u.UserId);
+        }
+    }
+}
---
+++ Mps.Domain/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Mps.Domain.Entities
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public required string FullName { get; set; }
+        public required string Email { get; set; }
+        public required string Role { get; set; }
+    }
+}
