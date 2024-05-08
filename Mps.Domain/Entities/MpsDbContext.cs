8686e015defbfeead9214182aed81040c432c573
HOANG Trung Thong <thoang5@mantu.com>
2024-05-08 17:47:46 +0700

    update status data

--- Mps.Domain/Entities/MpsDbContext.cs
+++ Mps.Domain/Entities/MpsDbContext.cs
@@ -55,10 +55,33 @@ namespace Mps.Domain.Entities
             modelBuilder.Entity<OrderDetail>().HasOne(o => o.Order).WithMany().HasForeignKey(o => o.OrderId);
 
             modelBuilder.Entity<OrderStatus>().HasKey(s => s.OrderStatusId);
+            modelBuilder.Entity<OrderStatus>().HasData(
+                new OrderStatus { OrderStatusId = 1, OrderStatusName = "Pending" },
+                new OrderStatus { OrderStatusId = 2, OrderStatusName = "Processing" },
+                new OrderStatus { OrderStatusId = 3, OrderStatusName = "Delivered" },
+                new OrderStatus { OrderStatusId = 4, OrderStatusName = "Cancelled" },
+                new OrderStatus { OrderStatusId = 5, OrderStatusName = "Returned" },
+                new OrderStatus { OrderStatusId = 6, OrderStatusName = "Refunded" },
+                new OrderStatus { OrderStatusId = 7, OrderStatusName = "Completed" }
+                );
 
             modelBuilder.Entity<PaymentStatus>().HasKey(s => s.PaymentStatusId);
+            modelBuilder.Entity<PaymentStatus>().HasData(
+                new PaymentStatus { PaymentStatusId = 1, PaymentStatusName = "Pending" },
+                new PaymentStatus { PaymentStatusId = 2, PaymentStatusName = "Processing" },
+                new PaymentStatus { PaymentStatusId = 3, PaymentStatusName = "Paid" },
+                new PaymentStatus { PaymentStatusId = 4, PaymentStatusName = "Cancelled" },
+                new PaymentStatus { PaymentStatusId = 5, PaymentStatusName = "Refunded" }
+                );
 
             modelBuilder.Entity<PaymentMethod>().HasKey(m => m.PaymentMethodId);
+            modelBuilder.Entity<PaymentMethod>().HasData(
+                new PaymentMethod { PaymentMethodId = 1, PaymentMethodName = "Cash on Delivery" },
+                new PaymentMethod { PaymentMethodId = 2, PaymentMethodName = "Credit Card" },
+                new PaymentMethod { PaymentMethodId = 3, PaymentMethodName = "Debit Card" },
+                new PaymentMethod { PaymentMethodId = 4, PaymentMethodName = "Net Banking" },
+                new PaymentMethod { PaymentMethodId = 5, PaymentMethodName = "UPI" }
+                );
 
         }
     }
