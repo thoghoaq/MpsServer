87207a02098a2cbc456477f26d5266b35241c12c
HOANG Trung Thong <thoang5@mantu.com>
2024-06-08 14:00:38 +0700

    orderby updated

--- Mps.Application/Features/Account/GetAllUsers.cs
+++ Mps.Application/Features/Account/GetAllUsers.cs
@@ -36,7 +36,9 @@ namespace Mps.Application.Features.Account
                 {
                     query.Skip((request.PageNumber.Value - 1) * request.PageSize.Value).Take(request.PageSize.Value);
                 }
-                var users = await query.ToListAsync(cancellationToken: cancellationToken);
+                var users = await query
+                    .OrderByDescending(u => u.UpdatedAt)
+                    .ToListAsync(cancellationToken: cancellationToken);
                 return CommandResult<Result>.Success(new Result { Users = users });
             }
         }
