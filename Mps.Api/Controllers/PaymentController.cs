f9a163da92f5b9b6d17b095953f04dbbe84a3081
HOANG Trung Thong <thoang5@mantu.com>
2024-06-18 22:36:16 +0700

    Revert "Revert "redirect to frontend page""
    This reverts commit fbddb13bfb76295e9afc7109b1d11f971cf16ecb.

--- Mps.Api/Controllers/PaymentController.cs
+++ Mps.Api/Controllers/PaymentController.cs
@@ -8,7 +8,7 @@ namespace Mps.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class PaymentController(IMediator mediator) : ControllerBase
+    public class PaymentController(IMediator mediator, IConfiguration configuration) : ControllerBase
     {
         private readonly IMediator _mediator = mediator;
 
@@ -69,10 +69,18 @@ namespace Mps.Api.Controllers
                 Vnp_ResponseCode = response.Vnp_ResponseCode,
                 Vnp_TmnCode = response.Vnp_TmnCode
             });
-            return processResult.IsSuccess ? Ok(processResult.Payload) : BadRequest(new
+
+            var webApp = configuration.GetSection("AllowedOrigins:MpsWebApp").Value;
+            string url = $"{webApp}/vnpay-return?success={processResult.IsSuccess}";
+            if (processResult.IsSuccess)
             {
-                reason = processResult.FailureReason
-            });
+                url += $"&paymentId={processResult.Payload?.PaymentId}";
+            }
+            else
+            {
+                url += $"&reason={processResult.FailureReason}";
+            }
+            return Redirect(url);
         }
 
         [HttpGet]
