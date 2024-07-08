b215469d1a82f00f8908524d94070cc1c150ea79
HOANG Trung Thong <thoang5@mantu.com>
2024-07-08 17:24:36 +0700

    field feedback

---
+++ Mps.Api/Migrations/20240708102405_IsFeedback.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mps.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class IsFeedback : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsFeedbacked",
+                table: "OrderDetails",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsFeedbacked",
+                table: "OrderDetails");
+        }
+    }
+}
