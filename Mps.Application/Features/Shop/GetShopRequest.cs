979a766391458c97b60d24b4612454f59e55e925
HOANG Trung Thong <thoang5@mantu.com>
2024-08-01 12:46:49 +0700

    get shop request

--- Mps.Application/Features/Shop/GetShopRequest.cs
+++ Mps.Application/Features/Shop/GetShopRequest.cs
@@ -18,7 +18,17 @@ namespace Mps.Application.Features.Shop
 
         public class Result
         {
-            public List<Domain.Entities.Shop> Shops { get; set; } = [];
+            public List<ShopResult> Shops { get; set; } = [];
+        }
+
+        public class ShopResult : Domain.Entities.Shop
+        {
+            public ShopOwner? ShopOwner { get; set; }
+        }
+
+        public class ShopOwnerResult : ShopOwner
+        {
+            public User? User { get; set; }
         }
 
         public class QueryHandler(MpsDbContext context, IAppLocalizer localizer, ILogger<GetShopRequest> logger) : IRequestHandler<Query, CommandResult<Result>>
@@ -28,6 +38,36 @@ namespace Mps.Application.Features.Shop
                 try
                 {
                     var query = context.Shops
+                        .Join(context.ShopOwners.Join(context.Users, u => u.UserId, x => x.Id, (u, x) => new { u, x }).Select(
+                                y => new ShopOwnerResult
+                                {
+                                    UserId = y.u.UserId,
+                                    User = y.x,
+                                    CreatedAt = y.u.CreatedAt,
+                                    UpdatedAt = y.u.UpdatedAt,
+                                }
+                            ), s => s.ShopOwnerId, so => so.UserId, (s, so) => new { s, so })
+                        .Select(s => new ShopResult
+                        {
+                            Address = s.s.Address,
+                            CreatedAt = s.s.CreatedAt,
+                            Id = s.s.Id,
+                            IsActive = s.s.IsActive,
+                            PhoneNumber = s.s.PhoneNumber,
+                            ShopName = s.s.ShopName,
+                            City = s.s.City,
+                            District = s.s.District,
+                            IsAccepted = s.s.IsAccepted,
+                            ShopOwner = s.so,
+                            Avatar = s.s.Avatar,
+                            Cover = s.s.Cover,
+                            ShopOwnerId = s.s.ShopOwnerId,
+                            UpdatedAt = s.s.UpdatedAt,
+                            Description = s.s.Description,
+                            Latitude = s.s.Latitude,
+                            Longitude = s.s.Longitude,
+                            PayPalAccount = s.s.PayPalAccount,
+                        })
                         .Where(s => !s.IsActive)
                         .Where(s => request.Filter == null || s.ShopName.Contains(request.Filter));
 
