10fe64e4250eca259ed853dd45734dbae8de6059
HOANG Trung Thong <thoang5@mantu.com>
2024-08-15 14:36:57 +0700

    update database

---
+++ Mps.Api/Migrations/20240815073428_AddPaymentInfo.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mps.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPaymentInfo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "OrderInfo",
+                table: "Payments",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TransactionNo",
+                table: "Payments",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OrderInfo",
+                table: "Payments");
+
+            migrationBuilder.DropColumn(
+                name: "TransactionNo",
+                table: "Payments");
+        }
+    }
+}
